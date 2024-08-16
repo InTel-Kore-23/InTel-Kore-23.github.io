38e7abec7120bccdc3448e8deda306b5c923ee76
Bùi Nguyễn Tấn Sang <endermatday@gmail.com>
2024-08-16 16:28:20 +0700

    Update script.js

--- journey/script.js
+++ journey/script.js
@@ -1,7 +1,7 @@
 var aText = new Array(
 "‼️ THÔNG BÁO ‼️ THÔNG BÁO ‼️", "Các quý hành khách vui lòng chú ý ‼️", "Tàu bay mang số hiệu IT-K23 bắt đầu khởi hành. Chuyến bay sẽ đưa các bạn vượt qua những tầng mây để khám phá vũ trụ Tin học và mã hoá hành trình đi đến tương lai.", "Vậy trên chuyến bay mang số hiệu IT-K23 có những ai?", "Cơ trưởng cô Lê Thị Minh Thư - người dìu dắt những cô cậu bé đam mê Tin học bằng sự dịu dàng lẫn sự nhiệt huyết.", "Cùng với cơ phó thầy Nguyễn Văn Như - người luôn lắng nghe và truyền cảm hứng cho mỗi bước tiến của từng ITK23ers.", "Và chúng mình là 35 hành khách mang trong mình những trái tim trẻ tràn đầy nhiệt huyết, say mê và không ngừng khám phá thế giới công nghệ đầy diệu kỳ. Trải qua bao thử thách và sự nỗ lực không ngừng nghỉ, giờ đây chúng mình đã cùng nhau đặt chân lên chiếc tàu bay này - nơi mà sự sáng tạo được thỏa sức bùng nổ mạnh mẽ, và tri thức trở thành “nguồn năng lượng” vững chắc để “lái” ước mơ bay cao và bay xa hơn nữa.", "Hãy đồng hành cùng tàu bay mang số hiệu IT-K23.", "Trên suốt chặng bay của tàu bay mang số hiệu IT-K23, chúng mình không chỉ mong muốn đến để được học hỏi, mà còn đến để kết nối những trái tim, sẻ chia những niềm vui, nỗi buồn và cùng nhau vun đắp những ước mơ lớn. Với ITK23ers, mỗi thử thách phía trước đều sẽ là một cơ hội để trở nên mạnh mẽ hơn và trưởng thành hơn.", "Chúng mình rất mong được các bạn dõi theo và cùng đồng hành trong chuyến bay đầy ý nghĩa này.", "HÃY NHẤN VÀO ĐÂY ĐỂ NHẬN LẤY CHIẾC VÉ ĐỒNG HÀNH CÙNG ITK23 NHÉ!!"
 );
-var iSpeed = 50;
+var iSpeed = 100;
 var iIndex = 0;
 var iArrLength = aText[0].length;
 var iScrollAt = 20;
